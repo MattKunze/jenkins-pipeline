f6c885cd27c362d742978b65d46f5b471342ee38
Matt Kunze <matt.kunze@datasplice.com>
2018-01-29 13:39:25 -0700

    and fix syntax error

--- packages/node-app/src/App.test.js
+++ packages/node-app/src/App.test.js
@@ -7,6 +7,6 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-it('has a failing test', => {
+it('has a failing test', () => {
   throw new Error('It done blew up');
 })
